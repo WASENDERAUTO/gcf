ebfa6194a40f113c893e6d1d160b46ce20bcd26e
Jxxzyshn <71040617+jxxzy@users.noreply.github.com>
2024-02-03 18:14:27 +0700

    Update function.go
    .

--- login/function.go
+++ login/function.go
@@ -15,7 +15,7 @@ func init() {
 func wasenderauto_Login(w http.ResponseWriter, r *http.Request) {
 	// Set CORS headers for the preflight request
 	if r.Method == http.MethodOptions {
-		w.Header().Set("Access-Control-Allow-Origin", "https://wasenderauto.github.io")
+		w.Header().Set("Access-Control-Allow-Origin", "https://cyberoren.my.id")
 		w.Header().Set("Access-Control-Allow-Methods", "POST")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
 		w.Header().Set("Access-Control-Max-Age", "3600")
@@ -23,7 +23,7 @@ func wasenderauto_Login(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	// Set CORS headers for the main request.
-	w.Header().Set("Access-Control-Allow-Origin", "https://wasenderauto.github.io")
+	w.Header().Set("Access-Control-Allow-Origin", "https://cyberoren.my.id")
 	fmt.Fprintf(w, backren.LogIn("PASETOPRIVATEKEY", r))
 
 }
